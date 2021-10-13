5516374707bacf4be97ce7e7ab035e41eb9e9887
Priyankajumwal <89975607+Priyankajumwal@users.noreply.github.com>
2021-10-13 15:44:52 +0530

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('login', Login.as_view(), name='login'),
     path('logout', logout , name='logout'),
     path('cart', Cart.as_view(), name='cart'),
-    path('check-out', CheckOut.as_view(), name='checkout'),
+    path('check-out', auth_middleware(CheckOut.as_view()), name='checkout'),
     path('orders', auth_middleware(Orderview.as_view()), name='orders'),
 ]
